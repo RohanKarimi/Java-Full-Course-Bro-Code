a357d6bfcc897246a7288fc6ff727364ae38c26c
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-06 12:29:17 -0300

    new Java Project

---
+++ 1) javaTutorialForBeginners/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args){
+        
+        System.out.println("\tI love pizza\n");
+        System.out.println("Its really good!");
+
+
+
+
+
+        //   \n break line
+        //   \t horizontal space
+        //   \" print double quotes
+        //   \\ print backslash
+
+        // anything after a backslash is a scape sequence.
+
+        
+
+    }
+}
