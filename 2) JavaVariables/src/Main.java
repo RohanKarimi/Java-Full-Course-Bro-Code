b911d17d0a4cfe509e44d3f9ec0451f2c76248a5
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-06 12:53:27 -0300

    main class

--- 2) JavaVariables/src/Main.java
+++ 2) JavaVariables/src/Main.java
@@ -2,6 +2,26 @@ public class Main{
 
     public static void main(String[] args) {
         
+        // int x;   declaration
+
+        // x = 123;   assignment
+
+        int x = 123;  // initialization
+        System.out.println("My number is: "+ x);
+
+        // long y = 18383993498348489L;
+
+        float y = 3.14f;
+        double w = 3.14;
+        boolean z = true;
+
+        char symbol = '@';
+
+        String name = "Bro";
+
+        System.out.println("Hello "+name);
+
+        
         
     }
 }
