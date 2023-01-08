de51fa1fe83c18fab8b37a33a61afe92407ea7ad
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-08 15:23:32 -0300

    main

--- 9) IfStatements/src/Main.java
+++ 9) IfStatements/src/Main.java
@@ -4,8 +4,20 @@ public class Main {
         
         // if statement = performs a block of code if it´s condition evaluates to be true
 
-        
+        int age = 18;
 
+
+        if(age >= 75){
+            System.out.println("Ok Boomer!");
+        }
+        else if(age >= 18){
+            System.out.println("You are an adult!");
+        }else if(age >= 13){
+            System.out.println("You are a teenager!");
+        }
+        else{
+            System.out.println("You are not an adult!");
+        }
     }
     
 }
