fb02e3a6eeccaa0ae8c4633b43bd28202a650978
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-08 18:02:54 -0300

    for each

--- 21) ForEachLoop/src/ForEachLoop.java
+++ 21) ForEachLoop/src/ForEachLoop.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class ForEachLoop {
 
     public static void main(String[] args) {
@@ -5,6 +7,23 @@ public class ForEachLoop {
         // for-each = traversing technique to iterate through the elements in an array/collection
         //            less steps, more readable
         //            less flexible
+
+
+        ArrayList<String> animalsList = new ArrayList<>();
+        animalsList.add("bird");
+        animalsList.add("rat");
+        animalsList.add("dog");
+        animalsList.add("cat");
+
+        for(String i : animalsList){   // for every string "i" in our arrayList(animalsList)
+            System.out.println(i);
+        }
+
+
+        String[] animals = {"cat", "dog","rat", "bird"};
+        for(String i : animals){   // for every string "i" in our array(animals)
+            System.out.println(i);
+        }
     }
     
 }
