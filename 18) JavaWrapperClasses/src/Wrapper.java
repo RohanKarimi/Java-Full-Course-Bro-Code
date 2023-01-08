9fef658ba8f8e813171f9df1d7ff7764aff94032
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-08 17:29:07 -0300

    wrapper

--- 18) JavaWrapperClasses/src/Wrapper.java
+++ 18) JavaWrapperClasses/src/Wrapper.java
@@ -14,7 +14,27 @@ public class Wrapper {
         // int             Integer
         // double          Double
 
-        // autoboxing = the automatic conversion that the Java compiler makes 
+        // autoboxing = the automatic conversion that the Java compiler makes between the primitive types
+        //              and their corresponding object wrapper classes.
+
+        // unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primitive
+
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Bro";
+
+        // When we directly assign some values to each of these reference data types, what we have done is autoboxing: We automaticaly
+        // converted these primitive values to their corresponding wrapper class.
+
+        if(b == '@'){
+            System.out.println("This is true");
+        }
+
+        // variable b behaves like a normal primitive boolean (unboxing). You can still treat these wrapper as primitives.
+
+        
     }
     
 }
