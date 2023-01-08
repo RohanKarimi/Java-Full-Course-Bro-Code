98ecf4ae0b484a483fd8ea2b163f8f56d340c3c0
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-08 18:23:16 -0300

    method

---
+++ 22) JavaMethods/src/JavaMethods.java
@@ -0,0 +1,31 @@
+public class JavaMethods {
+
+    public static void main(String[] args) {
+        
+        // method =  a block of code that is executed whenever it is called upon
+        int x = 3;
+        int y = 4;
+        int z = add(x, y);
+        System.out.println(z);
+
+        
+        String name = "Bro";
+        int age = 21;
+
+        hello(name, age);
+
+
+    }
+
+    static int add(int x, int y){
+
+        int sum = x + y;
+        return sum;
+    }
+
+    static void hello(String name, int age){
+        System.out.println("Hello "+ name);
+        System.out.println("You are "+age);
+    }
+    
+}
