15de1994ad37530a27b7df4249a62622706d6661
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-08 15:09:56 -0300

    math class

--- 7) JavaMathClass/src/Main.java
+++ 7) JavaMathClass/src/Main.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
-        
+        Scanner scanner = new Scanner(System.in);
+
+        double x, y, z;
+
+        System.out.println("Enter side x: ");
+        x = scanner.nextDouble();
+
+        System.out.println("Enter side y");
+        y = scanner.nextDouble();
+
+        z = Math.sqrt((x * x) + (y * y));
+
+        System.out.println("The hypotenuse is: "+z);
+
+        scanner.close();
+
+
     }
     
 }
