cf770921188e92583586ed962997cc7ac78612ed
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-08 15:59:59 -0300

    logical

--- 11) JavaLogicalOperators/src/LogicalOperators.java
+++ 11) JavaLogicalOperators/src/LogicalOperators.java
@@ -10,7 +10,24 @@ public class LogicalOperators {
         //      || = (OR) either condition must be true
         //      !  = (NOT) reverses boolean value of a condition
 
-        System.out.println("HI");
+        // int temp = 25;
+
+        // if(temp > 30){
+        //     System.out.println("It is hot outside");
+        // }else if(temp >= 20 && temp <= 30){
+        //     System.out.println("It is warm outside");
+        // }else{
+        //     System.out.println("It is cold");
+        // }
+
+        System.out.println("You are playing a game! press q or Q to quit");
+        String response = scan.nextLine();
+
+        if(response.equals("Q") || response.equals("q")){
+            System.out.println("You quit the game");
+        }else{
+            System.out.println("You are still playing the game *pew pew* ");
+        }
     }
     
 }
