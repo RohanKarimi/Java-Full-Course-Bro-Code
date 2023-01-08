034fb39cbb1b8a7f7e72ef1fd78332569b4ebc34
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-08 17:19:14 -0300

    comments

--- 18) JavaWrapperClasses/src/Wrapper.java
+++ 18) JavaWrapperClasses/src/Wrapper.java
@@ -2,6 +2,19 @@ public class Wrapper {
 
     public static void main(String[] args) {
         
+        // wrapper class = provides a way to use primitive data types as reference data types
+        //                 reference dara types contain useful methods
+        //                 can be used with collections (ex: Arraylist)
+        
+        
+        // primitive        wrapper
+        //---------         -------
+        // boolean         Boolean
+        // char            Character
+        // int             Integer
+        // double          Double
+
+        // autoboxing = the automatic conversion that the Java compiler makes 
     }
     
 }
