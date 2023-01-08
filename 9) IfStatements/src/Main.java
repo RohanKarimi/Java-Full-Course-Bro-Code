7cbaedfd0ddbdd1a977acb8f6adcce0d79703812
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-08 15:18:40 -0300

    main

---
+++ 9) IfStatements/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // if statement = performs a block of code if it´s condition evaluates to be true
+
+        
+    }
+    
+}
