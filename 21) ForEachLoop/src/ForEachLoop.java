4016c0b8f961322db5f0c2d7258f586878b8b324
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-08 17:58:49 -0300

    aa

---
+++ 21) ForEachLoop/src/ForEachLoop.java
@@ -0,0 +1,10 @@
+public class ForEachLoop {
+
+    public static void main(String[] args) {
+        
+        // for-each = traversing technique to iterate through the elements in an array/collection
+        //            less steps, more readable
+        //            less flexible
+    }
+    
+}
