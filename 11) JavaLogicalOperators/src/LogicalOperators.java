bbd91faa0418fdb9118d6b883f3eddf18dd9dd8e
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-08 15:42:28 -0300

    renaming

---
+++ 11) JavaLogicalOperators/src/LogicalOperators.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LogicalOperators {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // Logical operators = used to connect two or more expressions
+        //
+        //      && = (AND) both conditions must be true
+        //      || = (OR) either condition must be true
+        //      !  = (NOT) reverses boolean value of a condition
+
+        System.out.println("HI");
+    }
+    
+}
