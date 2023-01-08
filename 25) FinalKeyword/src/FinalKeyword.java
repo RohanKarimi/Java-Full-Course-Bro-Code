2503fb9b462f0588527b4a8e1d8b763851d322f5
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-08 18:59:07 -0300

    final keyword

---
+++ 25) FinalKeyword/src/FinalKeyword.java
@@ -0,0 +1,16 @@
+public class FinalKeyword {
+
+    public static void main(String[] args) {
+        final double PI = 3.14159;             
+
+        // pi = 4;
+
+        System.out.println(PI);
+    }
+    // anything that is declared as final, cannot be changed or updtaed later in your program.
+
+    
+
+
+    
+}
