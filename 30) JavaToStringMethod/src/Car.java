933e7f86d300a7aadff120db500a7ee84fe35d6b
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-09 13:32:49 -0300

    saved

---
+++ 30) JavaToStringMethod/src/Car.java
@@ -0,0 +1,7 @@
+public class Car {
+    
+    String make = "Ford";
+    String model = "Mustang";
+    String color = "red";
+    int year = 2021;
+}
---
+++ 30) JavaToStringMethod/src/JavaToString.java
@@ -0,0 +1,12 @@
+public class JavaToString {
+    
+    public static void main(String[] args) {
+        
+        // toString(): special method that all objects inherit, that returns a String that "textually represents" an object.
+        //             can be used both implicitly and explicitly
+
+        Car car = new Car();
+
+        
+    }
+}
