3d775c29896587a0fd0f00964b2f05cf5168a18b
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-09 11:44:07 -0300

    lindo

--- 26) JavaObjects/src/JavaObjects.java
+++ 26) JavaObjects/src/JavaObjects.java
@@ -5,5 +5,20 @@ public class JavaObjects {
 
     public static void main(String[] args) {
         
+        Car myCar1 = new Car();
+        Car myCar2 = new Car();
+
+        System.out.println(myCar1.model);
+        System.out.println(myCar1.make);
+        System.out.println();
+
+        myCar1.drive();
+        myCar1.brake();
+        System.out.println();
+
+        System.out.println(myCar2.model);
+        System.out.println(myCar2.make);
+
+        
     }
 }
