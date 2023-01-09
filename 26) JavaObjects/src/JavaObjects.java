0e32537c933cfcfb83bed578434e851eeefe2af0
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-09 11:39:44 -0300

    yes

---
+++ 26) JavaObjects/src/JavaObjects.java
@@ -0,0 +1,9 @@
+public class JavaObjects {
+    
+    // object = an instance of a class that may contain attributes and methods
+    // example: (phone, desk, computer, cup)...
+
+    public static void main(String[] args) {
+        
+    }
+}
