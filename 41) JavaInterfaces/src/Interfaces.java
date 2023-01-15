c19815daade63b1265a7ca8523605ae42361b1bf
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-15 15:04:07 -0300

    yesss

---
+++ 41) JavaInterfaces/src/Interfaces.java
@@ -0,0 +1,9 @@
+public class Interfaces {
+    
+    public static void main(String[] args) {
+        
+        // interface = a template that can be applied to a class.
+        //             similar to inheritance, but specifies what a class has/must do.
+        //             classes can apply more than one interface, inheritance is limited to 1 super
+    }
+}
