cdcf65a721d707fdbbed046358d129d07c04edd9
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-15 13:22:21 -0300

    news

--- 35) JavaMethodOverriding/src/MethodOverriding.java
+++ 35) JavaMethodOverriding/src/MethodOverriding.java
@@ -2,5 +2,13 @@ public class MethodOverriding {
     
     public static void main(String[] args) {
         
+        // method overriding = Declaring a method in a subclass, which is already present in a parent class.
+        //                     done so that a child class can give its own implementation
+
+        Animal animal = new Animal();
+        animal.speak();
+        
+        Dog dog = new Dog();
+        dog.speak();
     }
 }
