df1d0a4b120e1058a526d1fc651a7c39c4f5e937
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-15 13:15:37 -0300

    classes

--- 34) JavaInheritance/src/Vehicle.java
+++ 34) JavaInheritance/src/Vehicle.java
@@ -1,3 +1,12 @@
 public class Vehicle {
     
+    double speed;
+
+    void go(){
+        System.out.println("This vehycle is moving");
+    }
+
+    void stop(){
+        System.out.println("This vehycle is stopped");
+    }
 }
