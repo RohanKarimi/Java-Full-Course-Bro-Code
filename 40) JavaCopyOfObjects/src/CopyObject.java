f205972926b0bde5c186328c8db945b4befa8fb3
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-15 14:58:23 -0300

    copy

---
+++ 40) JavaCopyOfObjects/src/CopyObject.java
@@ -0,0 +1,8 @@
+public class CopyObject {
+    
+    public static void main(String[] args) {
+        
+        Car car1 = new Car("Chevrolet", "Camaro", 2021);
+        Car car2 = new Car("Ford", "Mustang", 2022);
+    }
+}
