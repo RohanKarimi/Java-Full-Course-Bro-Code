b7a48f59845a74874616ff66abc6a44f47b8d8e2
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-15 13:54:03 -0300

    new classes

---
+++ 37) JavaAbstraction/src/Abstraction.java
@@ -0,0 +1,14 @@
+public class Abstraction {
+    
+    public static void main(String[] args) {
+        
+        // abstract = abstract classes cannot be instantiated, but they can have a subclass
+        //            abstract methods are declared without an implementation(forces the child class to implement)
+        //            It adds a layer of security to your program
+
+
+        // Vehicle vehicle = new Vehicle();
+        Car car = new Car();
+        car.go();
+    }
+}
---
+++ 37) JavaAbstraction/src/Vehicle.java
@@ -0,0 +1,7 @@
+public abstract class Vehicle {
+    // vehicle is too abstract we don´t want it to be instantiated.
+
+    abstract void go();  // it forces us to implement this method in one of the child classes.
+
+    
+}
