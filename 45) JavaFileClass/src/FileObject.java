147c928ff330ac2b9d410e3ce92120855f649a39
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-16 11:49:35 -0300

    file

--- 45) JavaFileClass/src/FileObject.java
+++ 45) JavaFileClass/src/FileObject.java
@@ -6,10 +6,14 @@ public class FileObject {
         
         // file = an abstract representation of file and directory pathnames
 
-        File file = new File("batata");
+        File file = new File("C:/Learning/Java-Full-Course-Bro-Code/45) JavaFileClass/src/batata.txt");
 
         if(file.exists()){
             System.out.println("That file exists! :O!");
+            System.out.println(file.getPath());
+            System.out.println(file.getAbsolutePath());
+            System.out.println(file.isFile());
+            // file.delete();
         }
         else{
             System.out.println("That file does not exist");
